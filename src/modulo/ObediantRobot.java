095a58e79a54e4fa7f4bf2813f979cb51d1e50f6
CookieDoughYum <league@iMac.attlocal.net>
2018-10-08 16:29:52 -0700

    Finished Module 3

--- src/modulo/ObediantRobot.java
+++ src/modulo/ObediantRobot.java
@@ -1,21 +1,71 @@
 package modulo;
 
+import java.awt.Color;
+
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class ObediantRobot {
+	static Robot commander=new Robot();
+	
 public static void main(String[] args) {
-	Robot commander=new Robot();
 	commander.penDown();
-	for(int i=0; i<4; i++) {
-		drawSquare();
+	String Shape=JOptionPane.showInputDialog("What shape do you want?");
+	String ColorRobot=JOptionPane.showInputDialog("What color do you want?");
+	if(ColorRobot.equals("Red")) {
+		commander.setPenColor(Color.red);
 	}
+	else if(ColorRobot.equals("Orange")) {
+		commander.setPenColor(Color.orange);
+	}
+	else if(ColorRobot.equals("Blue")) {
+		commander.setPenColor(Color.blue);
+	}
+	else if(ColorRobot.equals("Green")) {
+		commander.setPenColor(Color.green);
+	}
+	else if(ColorRobot.equals("Yellow")) {
+		commander.setPenColor(Color.orange);
+	}
+	
+	if(Shape.equals("Square")) {
+		for(int i=0; i<4; i++) {
+			drawSquare();
+		}
+	}
+	if(Shape.equals("Triangle")) {
+		commander.turn(30);
+		for(int i=0; i<3; i++) {
+			drawTriangle();
+		}
+	}
+	if(Shape.equals("Circle")) {
+		for(int i=0; i<36; i++) {
+			drawCircle();
+		}
+	}
+
 		
 	}
 
-public static void drawSquare(String[] args) {
+public static void drawSquare() {//4 times
 	commander.setSpeed(100);
 	commander.move(100);
 	commander.turn(90);
+}
+public static void drawTriangle() {//3 times
+	commander.setSpeed(100);
+	commander.turn(120);
+	commander.move(100);
+
+}
+public static void drawCircle() {
+	commander.setSpeed(100);
+	commander.move(10);
+	commander.turn(10);
+}
+
 }
 
 
